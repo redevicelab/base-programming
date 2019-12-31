4cd64abdf2a8d90e5bb2bb222574438de6613fbf
Redevice <redevice7@gmail.com>
2019-12-31 15:24:19 +0600

    homework_day_1

---
+++ hm_day_1/task_1_1.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+
+using namespace std;
+
+int main() 
+{
+	setlocale(LC_ALL, "ru");
+    	cout << "Михальчук\n";
+	cout << "Александр\n";
+	cout << "Александрович\n";
+	return 0;
+}
+
+
+// Выведите на экран, Вашу фамилию, имя и отчество, каждое слово с новой строки
---
+++ hm_day_1/task_1_4.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+
+using namespace std; 
+
+int main() 
+{
+	setlocale(LC_ALL, "ru");
+	cout << "С:\\project\\test.doc\n";
+	return 0; 
+}
+
+// Вывести на экран следующий текст:  С:\project\test.doc
