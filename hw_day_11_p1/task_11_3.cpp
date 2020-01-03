ca7b98a408243755feac0d8cb9c7d534c5eda074
Redevice <redevice7@gmail.com>
2020-01-04 01:13:55 +0600

    homework_day_11_p1

--- hw_day_11_p1/task_11_3.cpp
+++ hw_day_11_p1/task_11_3.cpp
@@ -12,28 +12,33 @@ using namespace std;
 
 const int ROW = 5;
 const int COL = 5;
+const int RANGE = 51;
+const int SHIFT = 1;
+const int FACTOR_FOR_FILL_ARRAY = 2;
+const int FOR_ODDITY_CHECKING = 2;
 
 int main()
-{	
+{
 	setlocale(LC_ALL, "ru");
 	srand(time(NULL));
-	int firstArr[ROW][COL * 2];
+	int firstArr[ROW][COL * FACTOR_FOR_FILL_ARRAY];
 	int secondArr[ROW][COL];
 
+	cout << "Вывод первного массива: \n";
 	for (int i = 0; i < ROW; i++)
 	{
-		for (int j = 0; j < COL * 2; j++)
+		for (int j = 0; j < COL * FACTOR_FOR_FILL_ARRAY; j++)
 		{
-			firstArr[i][j] = rand()%50;
+			firstArr[i][j] = rand() % RANGE;
 			cout << firstArr[i][j] << "\t";
-			if (j % 2 != 0) {
-				secondArr[i][j / 2] = firstArr[i][j - 1] + firstArr[i][j];
+			if (j % FOR_ODDITY_CHECKING != 0) {
+				secondArr[i][j / FACTOR_FOR_FILL_ARRAY] = firstArr[i][j - SHIFT] + firstArr[i][j];
 			}
 		}
 		cout << endl;
 	}
 
-	cout << "\nВторой массив\n";
+	cout << "Вывод второго массива: \n";
 	for (int i = 0; i < ROW; i++)
 	{
 		for (int j = 0; j < COL; j++)
